6b328b2d9d64e2154e66dc77d63691b9963549a1
bangdc <60103759+polopalay@users.noreply.github.com>
2022-12-16 23:52:49 +0700

    fix menu

--- src/components/menu/AppTopbar.js
+++ src/components/menu/AppTopbar.js
@@ -1,13 +1,23 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/button-has-type */
 /* eslint-disable max-lines-per-function */
-import React from 'react'
+import React, { useState } from 'react'
 import { classNames } from 'primereact/utils'
+import { useSelector } from 'react-redux'
+import { getAuth } from 'firebase/auth'
 import AppMenu from './AppMenu'
 import history from '../../History'
 import { appName } from '../../constant/config'
+import UpdateUser from './UpdateUser'
+import UserInfo from './UserInfo'
 
 const AppTopbar = (props) => {
+  const app = useSelector((state) => state.firebase.app)
+  const [currentUser, setCurrentUser] = useState(null)
+  const auth = getAuth(app)
+  auth.onAuthStateChanged((user) => {
+    setCurrentUser(user)
+  })
   return (
     <>
       <div className="layout-topbar">
@@ -47,16 +57,36 @@ const AppTopbar = (props) => {
               })}
             >
               <button className="p-link" onClick={props.onTopbarItemClick}>
-                <img src="/assets/layout/images/avatar.png" alt="profile" />
+                <img
+                  src={(currentUser && currentUser.photoURL) || '/assets/layout/images/avatar.png'}
+                  alt="profile"
+                />
               </button>
 
               <ul className="fadeInDown">
-                <li role="menuitem">
-                  <button className="p-link" onClick={() => history.push('/logout')}>
-                    <i className="pi pi-fw pi-sign-out" />
-                    <span>Logout</span>
-                  </button>
-                </li>
+                {currentUser ? (
+                  <>
+                    <li role="menuitem">
+                      <button className="p-link">
+                        <i className="pi pi-sign-out pi-fw" />
+                        <span onClick={() => history.push('/logout')}>Đăng xuất</span>
+                      </button>
+                    </li>
+                    <li role="menuitem">
+                      <UpdateUser currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                    </li>
+                    <li role="menuitem">
+                      <UserInfo currentUser={currentUser} />
+                    </li>
+                  </>
+                ) : (
+                  <li role="menuitem">
+                    <button className="p-link">
+                      <i className="pi pi-sign-in pi-fw" />
+                      <span onClick={() => history.push('/login')}>Đăng nhập</span>
+                    </button>
+                  </li>
+                )}
               </ul>
             </li>
           </ul>
